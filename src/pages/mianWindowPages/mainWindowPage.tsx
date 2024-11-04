53ab619b5d13b06bce7b411097cdacbf0cd062a3
WBY1026 <wby15010285159@163.com>
2024-11-04 16:07:28 +0800

    组件化的进一步处理

---
+++ src/pages/mianWindowPages/mainWindowPage.tsx
@@ -0,0 +1,8 @@
+
+function MainWindowPage() {
+    return (
+        <div>主窗口入口</div>
+    )
+}
+
+export default MainWindowPage;
--- src/windows/mainWindow/mainWindow.tsx
+++ src/windows/mainWindow/mainWindow.tsx
@@ -1,5 +1,8 @@
 import ReactDOM from "react-dom/client";
+// 导入页面
+import MainWindowPage from "@/pages/mianWindowPages/mainWindowPage";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <div>react online</div>
+    <MainWindowPage />
 )
